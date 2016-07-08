e019dd504a794978c41e34f229d7f4a04b0bff11
Alessandro Arnodo <alessandro@arnodo.net>
2016-07-08 14:25:46 +0200

    feat(app): adding global shortcut

--- main/index.js
+++ main/index.js
@@ -1,10 +1,33 @@
 console.time('init')
 
 const menubar = require('menubar')
-const { Menu, shell, dialog } = require('electron')
+const { Menu, shell, dialog, globalShortcut } = require('electron')
 const { INDEX_PATH, TRAY_ICON_PATH } = require('./constants')
 const updater = require('./updater')
 const createMenuTemplate = require('./menuTemplate')
+
+function registerGlobalShortcut (accelerator, mb) {
+  globalShortcut.unregisterAll()
+  globalShortcut.register(accelerator, () => {
+    mb.window.isVisible() ? mb.hideWindow() : mb.showWindow()
+  })
+}
+
+function checkForUpdates () {
+  updater((err, newVersion) => {
+    if (err) return
+    const confirm = dialog.showMessageBox({
+      type: 'info',
+      title: 'Update available',
+      message: `A new version (${newVersion}) of Todoo is available.`,
+      detail: 'Do you want to download it now?',
+      buttons: ['Yes', 'No']
+    })
+    if (confirm === 0) {
+      shell.openExternal('https://github.com/vesparny/todoo/releases')
+    }
+  })
+}
 
 process.on('uncaughtException', function (err) {
   dialog.showErrorBox('Uncaught Exception: ' + err.message, err.stack || '')
@@ -24,24 +47,16 @@ mb.on('ready', () => {
   console.log('app is ready')
   const menu = Menu.buildFromTemplate(createMenuTemplate(mb, shell))
   Menu.setApplicationMenu(menu)
-
-  updater((err, newVersion) => {
-    if (err) return
-    const confirm = dialog.showMessageBox({
-      type: 'info',
-      title: 'Update available',
-      message: `A new version (${newVersion}) of Todoo is available.`,
-      detail: 'Do you want to download it now?',
-      buttons: ['Yes', 'No']
-    })
-    if (confirm === 0) {
-      shell.openExternal('https://github.com/vesparny/todoo/releases')
-    }
-  })
+  checkForUpdates()
+  registerGlobalShortcut('control+shift+space')
   mb.showWindow()
   console.timeEnd('init')
 })
 
 mb.on('after-create-window', () => {
   console.log('window')
+})
+
+mb.app.on('will-quit', function () {
+  globalShortcut.unregisterAll()
 })
