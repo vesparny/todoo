3d18d670562bbe150a54fd73f324f0fd4caa2830
Alessandro Arnodo <alessandro@arnodo.net>
2016-07-08 10:29:22 +0200

    fix(app): hide menu

--- main/index.js
+++ main/index.js
@@ -2,6 +2,7 @@ const menubar = require('menubar')
 const { Menu, shell, dialog } = require('electron')
 const { INDEX_PATH, TRAY_ICON_PATH } = require('./constants')
 const updater = require('./updater')
+const createMenuTemplate = require('./menuTemplate')
 
 const mb = menubar({
   'preload-window': true,
@@ -9,104 +10,12 @@ const mb = menubar({
   width: 600,
   height: 700,
   icon: TRAY_ICON_PATH,
-  'show-dock-icon': true
+  'show-dock-icon': false
 })
-
-const template = [{
-  label: 'Todoo',
-  submenu: [{
-    label: 'About Todoo',
-    role: 'about'
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Reload',
-    accelerator: 'CmdOrCtrl+R',
-    click: (item, focusedWindow) => {
-      if (focusedWindow) focusedWindow.reload()
-    }
-  }, {
-    label: 'Preferences...',
-    accelerator: 'Command+,',
-    click: () => {
-      mb.window.webContents.send('open-preferences')
-    }
-  }, {
-    label: 'Quit App',
-    accelerator: 'Command+Q',
-    selector: 'terminate:'
-  }, {
-    label: 'Toggle DevTools',
-    accelerator: 'Alt+Command+I',
-    click: () => {
-      mb.window.toggleDevTools()
-    }
-  }]
-}, {
-  label: 'Edit',
-  submenu: [{
-    label: 'Undo',
-    accelerator: 'CmdOrCtrl+Z',
-    role: 'undo'
-  }, {
-    label: 'Redo',
-    accelerator: 'Shift+CmdOrCtrl+Z',
-    role: 'redo'
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Cut',
-    accelerator: 'CmdOrCtrl+X',
-    role: 'cut'
-  }, {
-    label: 'Copy',
-    accelerator: 'CmdOrCtrl+C',
-    role: 'copy'
-  }, {
-    label: 'Paste',
-    accelerator: 'CmdOrCtrl+V',
-    role: 'paste'
-  }, {
-    label: 'Select All',
-    accelerator: 'CmdOrCtrl+A',
-    role: 'selectall'
-  }]
-}, {
-  label: 'Help',
-  role: 'help',
-  submenu: [{
-    label: 'Learn More',
-    click: () => {
-      shell.openExternal('https://github.com/vesparny/todoo#readme')
-    }
-  }, {
-    label: 'Report Issue',
-    click: () => {
-      shell.openExternal('https://github.com/vesparny/todoo/issues')
-    }
-  }, {
-    label: 'Source Code on GitHub',
-    click: () => {
-      shell.openExternal('https://github.com/vesparny/todoo')
-    }
-  }, {
-    label: 'Changelog',
-    click: () => {
-      shell.openExternal('https://github.com/vesparny/todoo/blob/master/CHANGELOG.md')
-    }
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Follow @vesparny on Twitter',
-    click: () => {
-      shell.openExternal('https://twitter.com/vesparny')
-    }
-  }]
-}]
 
 mb.on('ready', () => {
   console.log('app is ready')
-  const menu = Menu.buildFromTemplate(template)
+  const menu = Menu.buildFromTemplate(createMenuTemplate(mb, shell))
   Menu.setApplicationMenu(menu)
 
   updater((err, newVersion) => {
---
+++ main/menuTemplate.js
@@ -0,0 +1,94 @@
+
+module.exports = function createMenuTemplate (mb, shell) {
+  return [{
+    label: 'Todoo',
+    submenu: [{
+      label: 'About Todoo',
+      role: 'about'
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Reload',
+      accelerator: 'CmdOrCtrl+R',
+      click: (item, focusedWindow) => {
+        if (focusedWindow) focusedWindow.reload()
+      }
+    }, {
+      label: 'Preferences...',
+      accelerator: 'Command+,',
+      click: () => {
+        mb.window.webContents.send('open-preferences')
+      }
+    }, {
+      label: 'Quit App',
+      accelerator: 'Command+Q',
+      selector: 'terminate:'
+    }, {
+      label: 'Toggle DevTools',
+      accelerator: 'Alt+Command+I',
+      click: () => {
+        mb.window.toggleDevTools()
+      }
+    }]
+  }, {
+    label: 'Edit',
+    submenu: [{
+      label: 'Undo',
+      accelerator: 'CmdOrCtrl+Z',
+      role: 'undo'
+    }, {
+      label: 'Redo',
+      accelerator: 'Shift+CmdOrCtrl+Z',
+      role: 'redo'
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Cut',
+      accelerator: 'CmdOrCtrl+X',
+      role: 'cut'
+    }, {
+      label: 'Copy',
+      accelerator: 'CmdOrCtrl+C',
+      role: 'copy'
+    }, {
+      label: 'Paste',
+      accelerator: 'CmdOrCtrl+V',
+      role: 'paste'
+    }, {
+      label: 'Select All',
+      accelerator: 'CmdOrCtrl+A',
+      role: 'selectall'
+    }]
+  }, {
+    label: 'Help',
+    role: 'help',
+    submenu: [{
+      label: 'Learn More',
+      click: () => {
+        shell.openExternal('https://github.com/vesparny/todoo#readme')
+      }
+    }, {
+      label: 'Report Issue',
+      click: () => {
+        shell.openExternal('https://github.com/vesparny/todoo/issues')
+      }
+    }, {
+      label: 'Source Code on GitHub',
+      click: () => {
+        shell.openExternal('https://github.com/vesparny/todoo')
+      }
+    }, {
+      label: 'Changelog',
+      click: () => {
+        shell.openExternal('https://github.com/vesparny/todoo/blob/master/CHANGELOG.md')
+      }
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Follow @vesparny on Twitter',
+      click: () => {
+        shell.openExternal('https://twitter.com/vesparny')
+      }
+    }]
+  }]
+}
