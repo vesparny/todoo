5f2a9ffc30dc8f692602c0c43a2adf5d5d4540e3
Alessandro Arnodo <alessandro@arnodo.net>
2016-07-13 14:19:40 +0200

    chore(app): remove defaultvalue

--- renderer/containers/Settings.js
+++ renderer/containers/Settings.js
@@ -101,7 +101,7 @@ class Settings extends Component {
                 readOnly='readonly'
                 id='todooJsonDir'
                 onClick={this.openDialog}
-                defaultValue={todooJsonDir} />
+                value={todooJsonDir} />
               <button onClick={this.openDialog} className='task-item__button' type='button' key='bs'>
                 <svg className='icon' fill='#000000' height='24' viewBox='0 0 24 24' width='24'>
                   <path d='M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z' />
