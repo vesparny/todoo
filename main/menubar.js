054b62677e9a65e8b34426a203afe8f202c693d3
Alessandro Arnodo <alessandro@arnodo.net>
2016-07-13 13:51:31 +0200

    refactor(code): remove useless quotes

--- main/menubar.js
+++ main/menubar.js
@@ -2,12 +2,12 @@ const menubar = require('menubar')
 const { INDEX_PATH, TRAY_ICON_PATH } = require('../constants')
 
 const mb = menubar({
-  'preloadWindow': true,
+  preloadWindow: true,
   index: INDEX_PATH,
   width: 600,
   height: 700,
   icon: TRAY_ICON_PATH,
-  'showDockIcon': false
+  showDockIcon: false
 })
 
 module.exports = mb
